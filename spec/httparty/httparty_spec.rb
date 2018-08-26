e22e676ce6b4c1315dc0363fc201f8eca1921ef0
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2018-08-26 13:50:30 -0300

    Modules of record VCR

--- spec/httparty/httparty_spec.rb
+++ spec/httparty/httparty_spec.rb
@@ -8,4 +8,11 @@ describe 'HTTParty' do
     content_type = response.headers['content-type']
     expect(content_type).to match(/application\/json/)
   end
+  it 'content-type - new episodes', vcr: { cassette_name: 'jsonplaceholder/posts', record: :new_episodes } do
+    # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+    #      to_return(status: 200, body: "", headers: { 'content-type': 'application/json'})
+    response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/#{[1,2,3,4,5].sample}")
+    content_type = response.headers['content-type']
+    expect(content_type).to match(/application\/json/)
+  end
 end
